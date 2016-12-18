5855bf53cd5367d111151a9d0a75f4e65efb9978
Greg Perlman <gperlman27@gmail.com>
2016-12-18 00:25:44 +0000

    finish vigenere cipher

--- pset2/vigenere.c
+++ pset2/vigenere.c
@@ -8,6 +8,8 @@
 #define ARGCOUNT 2
 
 bool checkArgs(int n, int l);
+int keyAssign(int k);
+int encrypt(char l, int k);
 
 int main(int argc, string argv[]){
     // make sure argv[1] exists
@@ -28,11 +30,25 @@ int main(int argc, string argv[]){
     for(int i = 0, n = strlen(key); i < n; i++){
         if(!isalpha(key[i])){
             printf("No numeric values allowed\n");
-            return 0;
+            return 1;
         }
     }
     
-    printf("no errors yet!");
+    string input = GetString();
+    int keyLength = strlen(key);
+    int skipCount = 0;
+    
+    if (input != NULL){
+        for (int i = 0, n = strlen(input); i < n; i++){
+            if(isalpha(input[i])){
+                input[i] = encrypt(input[i], keyAssign((int) key[( i - skipCount) % keyLength]));
+            } else {
+                skipCount++;
+            }
+        }
+    }
+    
+    printf("%s\n", input);
     
     return 0;
 }
@@ -44,4 +60,35 @@ bool checkArgs(int n, int length){
     } else {
         return false;
     }
+}
+
+// Allows shift to be 2^31 - 26
+int keyAssign(int keyLetter){
+    if(keyLetter > 64 && keyLetter < 91){
+        keyLetter %= 65;
+    } else if(keyLetter > 96 && keyLetter < 123){
+        keyLetter %= 97;
+    }
+    
+    return keyLetter;
+}
+
+int encrypt(char letter, int shift){
+    int encrypted = (int) letter;
+    
+    if(encrypted > 64 && encrypted < 91){
+        if(encrypted + shift > 90){
+            encrypted = encrypted + shift - 26;
+        } else {
+            encrypted += shift;
+        }
+    } else if(encrypted > 96 && encrypted < 123){
+        if(encrypted + shift > 122){
+            encrypted = encrypted + shift -26;
+        } else {
+            encrypted += shift;
+        }
+    }
+    
+    return encrypted;
 }
